0f3b1c9bfd5fc1df08e792b0cd055c331bea0bbe
Hardik Joshi <44543811+HardikJ7321@users.noreply.github.com>
2020-07-02 21:37:22 +0530

    Update index.js

--- views/In-Line Embed Mode/index.js
+++ views/In-Line Embed Mode/index.js
@@ -57,28 +57,28 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
                     hitType: 'event',
                     eventCategory: 'pdf',
                     eventAction: 'document_open',
-                    eventValue:event.data.fileName
+                    eventLabel:event.data.fileName
                   });
                 break;
                 case 'PAGE_VIEW' : ga('send', {
                     hitType: 'event',
                     eventCategory: 'pdf',
                     eventAction: 'page_view',
-                    eventValue:`${event.data.pageNumber},${event.data.fileName}`
+                    eventLabel:`${event.data.pageNumber},${event.data.fileName}`
                   });
                 break;
                 case 'DOCUMENT_DOWNLOAD': ga('send', {
                     hitType: 'event',
                     eventCategory: 'pdf',
                     eventAction: 'document_download',
-                    eventValue:event.data.fileName
+                    eventLabel:event.data.fileName
                   });
                 break;
                 case 'TEXT_COPY' :  ga('send', {
                     hitType: 'event',
                     eventCategory: 'pdf',
                     eventAction: 'text_copy',
-                    eventValue:`${event.data.copiedText},${event.data.fileName}`
+                    eventLabel:`${event.data.copiedText},${event.data.fileName}`
                   });
                 break;
                 default: ;
